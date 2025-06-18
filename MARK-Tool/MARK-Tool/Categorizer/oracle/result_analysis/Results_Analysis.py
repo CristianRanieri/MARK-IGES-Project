9e6dd1d4c04fb111084d981e05ea2966565054b9
Cristian <c.ranieri7@studenti.unisa.it>
2025-06-18 20:56:32 +0200

    Add marge for consumer and producer, add errors of download repos

--- MARK-Tool/MARK-Tool/Categorizer/oracle/result_analysis/Results_Analysis.py
+++ MARK-Tool/MARK-Tool/Categorizer/oracle/result_analysis/Results_Analysis.py
@@ -11,8 +11,10 @@ import pandas as pd
 # Per ogni cartella esaminata crea un file(come result_consumer_1.csv) dove salva il contenuto di 'oracle' con la nuova colonna aggiunta.
 #-------------------------------------------------------------------------------------------------------
 
+
 # Load the oracle.csv file
 oracle_df = pd.read_csv("../oracle_producer.csv")
+
 
 ### First Dataset ###
 # Create a new dataframe with the required columns
@@ -37,6 +39,7 @@ for filename in os.listdir(producers_folder):
 
 # Save the resulting dataframe to a new CSV file
 result_df.to_csv("result_producer_1.csv", index=False)
+### First Dataset ###
 
 
 ### Second Dataset ###
@@ -62,10 +65,45 @@ for filename in os.listdir(producers_folder):
 
 # Save the resulting dataframe to a new CSV file
 result_df.to_csv("result_producer_2.csv", index=False)
+### Second Dataset ###
+
+
+
+oracle_df = pd.read_csv("../oracle_producer_new.csv")
+
+### Third Dataset ###
+# Create a new dataframe with the required columns
+result_df = oracle_df[["ProjectName", "Is_Real_ML_producer"]].copy()
+
+# Add the "is ML Producer" column
+result_df["is ML Producer"] = "No"
+
+# Path to the folder containing Producers_1 CSV files
+producers_folder = "../../src/Producers/Producers_3"
+
+# Check each .csv file in the Producers_1 folder
+for filename in os.listdir(producers_folder):
+    if filename.endswith(".csv"):
+        # Read the current CSV file
+        file_path = os.path.join(producers_folder, filename)
+        producer_df = pd.read_csv(file_path)
+
+        # Check for matching ProjectName values
+        matching_projects = result_df["ProjectName"].isin(producer_df["ProjectName"])
+        result_df.loc[matching_projects, "is ML Producer"] = "Yes"
+
+# Save the resulting dataframe to a new CSV file
+result_df.to_csv("result_producer_3.csv", index=False)
+### Third Dataset ###
+
+
+
+
 
 
 # Load the oracle.csv file
 oracle_df = pd.read_csv("../oracle_consumer.csv")
+
 
 ### First Dataset ###
 # Create a new dataframe with the required columns
@@ -90,6 +128,7 @@ for filename in os.listdir(producers_folder):
 
 # Save the resulting dataframe to a new CSV file
 result_df.to_csv("result_consumer_1.csv", index=False)
+### First Dataset ###
 
 
 ### Second Dataset ###
@@ -115,6 +154,7 @@ for filename in os.listdir(producers_folder):
 
 # Save the resulting dataframe to a new CSV file
 result_df.to_csv("result_consumer_2.csv", index=False)
+### Second Dataset ###
 
 
 ### Third Dataset ###
@@ -140,6 +180,7 @@ for filename in os.listdir(producers_folder):
 
 # Save the resulting dataframe to a new CSV file
 result_df.to_csv("result_consumer_3.csv", index=False)
+### Third Dataset ###
 
 
 ### Fourth Dataset ###
@@ -164,6 +205,8 @@ for filename in os.listdir(producers_folder):
         result_df.loc[matching_projects, "is ML Consumer"] = "Yes"
 # Save the resulting dataframe to a new CSV file
 result_df.to_csv("result_consumer_4.csv", index=False)
+### Fourth Dataset ###
+
 
 ### Fifth Dataset ###
 # Create a new dataframe with the required columns
@@ -187,3 +230,34 @@ for filename in os.listdir(producers_folder):
         result_df.loc[matching_projects, "is ML Consumer"] = "Yes"
 # Save the resulting dataframe to a new CSV file
 result_df.to_csv("result_consumer_5.csv", index=False)
+### Fifth Dataset ###
+
+
+
+# Load the oracle.csv file
+oracle_df = pd.read_csv("../oracle_consumer_new.csv")
+
+
+### Sixth Dataset ###
+# Create a new dataframe with the required columns
+result_df = oracle_df[["ProjectName", "Is_Real_ML_consumer"]].copy()
+
+# Add the "is ML Producer" column
+result_df["is ML Consumer"] = "No"
+
+# Path to the folder containing Producers_1 CSV files
+producers_folder = "../../src/Consumers/Consumers_6"
+
+# Check each .csv file in the Producers_1 folder
+for filename in os.listdir(producers_folder):
+    if filename.endswith(".csv"):
+        # Read the current CSV file
+        file_path = os.path.join(producers_folder, filename)
+        producer_df = pd.read_csv(file_path)
+
+        # Check for matching ProjectName values
+        matching_projects = result_df["ProjectName"].isin(producer_df["ProjectName"])
+        result_df.loc[matching_projects, "is ML Consumer"] = "Yes"
+# Save the resulting dataframe to a new CSV file
+result_df.to_csv("result_consumer_6.csv", index=False)
+### Sixth Dataset ###
